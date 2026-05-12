e7cd12a75e770a611551718898f1467fa992de19
irensm27 <semenets.irina27@gmail.com>
2026-05-12 12:20:46 +0300

    Lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 
 import './App.css'
-import MyComponent from "./components/MyComponent.tsx";
+
 
 function App() {
 
   return (
     <>
-      <MyComponent text ={"Hello World!"} />
-      {/*{MyComponent({text: "Hello World!"})}*/}
+
     </>
   )
 }
