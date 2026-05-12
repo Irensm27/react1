75e3eb8f1f03c3f67485994b75e58b729b8202ea
irensm27 <semenets.irina27@gmail.com>
2026-05-12 16:43:45 +0300

    hw1/task1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-
 import './App.css'
+
+const coursesTitleArray:string[] = [
+  'JavaScript Complex',
+  'Java Complex',
+  'Python Complex',
+  'QA Complex',
+  'Fullstack',
+  'Frontend'
+];
 
 
 function App() {
 
   return (
     <>
+      <ul>
+        {coursesTitleArray.map((value, index) => <li key = {index}>{value}</li>)}
+      </ul>
 
     </>
   )
 }
 
 export default App
+
+
