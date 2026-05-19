aed99f9cb81a41a50a6c240b1716d97029f56dd5
irensm27 <semenets.irina27@gmail.com>
2026-05-19 18:46:38 +0300

    hw1/task4

--- src/arrayComponent/ArrayComponent.tsx
+++ src/arrayComponent/ArrayComponent.tsx
@@ -3,18 +3,20 @@ import type {ICousresArray} from "../model/model.ts";
 interface ArrayComponentProps {
     item: ICousresArray
 }
-export const ArrayComponent = ({item}: ArrayComponentProps) => {
-    return(
-        <div>
-            <h2>{item.title}</h2>
-            <p>Month - {item.monthDuration}</p>
-            <p>Hour - {item.hourDuration}</p>
-            <ul>
-                {item.modules.map((item, index) => {
-                    return <li key={index}>{item}</li>;
-                })}
+
+export const ArrayComponent = ({ item }: ArrayComponentProps) => {
+    return (
+        <div className="rounded-2xl p-5 text-white bg-linear-to-br from-purple-500 to-pink-500">
+            <h2 className="text-xl font-bold mb-2">{item.title}</h2>
+
+            <p>Month: {item.monthDuration}</p>
+            <p className="mb-3">Hours: {item.hourDuration}</p>
+
+            <ul className="space-y-1">
+                {item.modules.map((item, index) => (
+                    <li className="bg-white/20 px-2 py-1 rounded" key={index}>{item}</li>
+                ))}
             </ul>
         </div>
-    )
-
-}
+    );
+};
--- src/arrayComponents/ArrayComponents.tsx
+++ src/arrayComponents/ArrayComponents.tsx
@@ -6,9 +6,7 @@ export const ArrayComponents = () => {
         <div>
             {coursesArray.map((value, index) => {
                 return (
-                    <ArrayComponent key={index} item={value}>
-
-                    </ArrayComponent>
+                    <ArrayComponent key={index} item={value}/>
                 );
             })}
         </div>
