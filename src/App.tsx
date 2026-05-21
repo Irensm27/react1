d906f12c2cbf03574f9ca5f37a48f571e90ce46b
irensm27 <semenets.irina27@gmail.com>
2026-05-21 18:33:14 +0300

    Lesson2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+// import './App.css'
+
+import {useState} from "react";
 
 function App() {
+  // eslint-disable-next-line prefer-const
+  let [counter, setCounter] = useState<number>(0);
 
   return (
-    <>
-
-    </>
+    <div>
+        <h2>{counter}</h2>
+        <button onClick={() => {
+          setCounter(++counter);
+        }}>+</button>
+        <button onClick={() => {
+          setCounter(--counter);
+        }}>-</button>
+    </div>
   )
 }
 
