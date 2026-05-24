249ecaf20fc934c158c3aea0cc9c819644397050
irensm27 <semenets.irina27@gmail.com>
2026-05-24 14:38:27 +0300

    Lesson2

---
+++ src/services/api.service.ts
@@ -0,0 +1,12 @@
+import type {IUser} from "../models/IUser.ts";
+
+export const getUsers = async ():Promise<IUser[]> => {
+    const users = await fetch('http://jsonplaceholder.typicode.com/users')
+        .then(value => value.json())
+    return users;
+}
+export const getUser = async ():Promise<IUser> => {
+    const user = await fetch('http://jsonplaceholder.typicode.com/users/id')
+        .then(value => value.json())
+    return user;
+}
