90f704adf200a8e70ba32db39e6906a6e8cc5496
irensm27 <semenets.irina27@gmail.com>
2026-05-28 18:28:46 +0300

    Homework 2.3

--- src/components/comment-component/CommentComponent.tsx
+++ src/components/comment-component/CommentComponent.tsx
@@ -5,13 +5,23 @@ export interface ICommentProps {
     comment: ICommentModel;
 }
 export const CommentComponent: FC<ICommentProps> = ({comment: {id, name, email,body}}) => {
-    return
-    (
-        <div>
-            <h2>{id}</h2>
-            <h2>{name}</h2>
-            <p>{email}</p>
-            <p>{body}</p>
+    return (
+        <div className="bg-gradient-to-r from-purple-50 to-blue-50 p-4 rounded-xl shadow-md hover:shadow-xl transition border border-purple-200">
+
+            <p className="text-xs text-purple-500 mb-1">Comment #{id}</p>
+
+            <h3 className="text-lg font-semibold text-gray-800">
+                {name}
+            </h3>
+
+            <p className="text-sm text-blue-600 mb-2">
+                {email}
+            </p>
+
+            <p className="text-gray-700 leading-relaxed">
+                {body}
+            </p>
+
         </div>
     );
 };
