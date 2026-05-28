e05a93494d6443ed5816da1654fb4ff66ad76d1c
irensm27 <semenets.irina27@gmail.com>
2026-05-28 18:07:25 +0300

    Homework 2.3

--- src/components/comment-component/CommentComponent.tsx
+++ src/components/comment-component/CommentComponent.tsx
@@ -4,11 +4,12 @@ import type {ICommentModel} from "../../models/CommentModel.ts";
 export interface ICommentProps {
     comment: ICommentModel;
 }
-export const CommentComponent: FC<ICommentProps> = (comment: id, name, email,body) => {
+export const CommentComponent: FC<ICommentProps> = ({comment: id, name, email,body}) => {
     return
     {
         <div>
             <h2>{id}</h2>
+
 
         </div>
     }
