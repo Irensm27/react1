f02aef3de627b534992a92f81fffd72d87d8e71b
irensm27 <semenets.irina27@gmail.com>
2026-06-06 16:24:38 +0300

    Lesson 3.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,33 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+// import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {MainLayout} from "./layouts/MainLayout.tsx";
-import APage from "./pages/APage.tsx";
-import BPage from "./pages/BPage.tsx";
+// import APage from "./pages/APage.tsx";
+// import BPage from "./pages/BPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
+//
+// export const router = createBrowserRouter([
+//     {
+//         path:'/', element:<MainLayout/>,
+//         children:[
+//             {path:'', element:<HomePage />},
+//             {path:'a', element:<APage/>},
+//             {path:'b', element:<BPage/>},
+//
+//         ]
+//     },
+// ])
 
-export const router = createBrowserRouter([
-    {
-        path:'/', element:<MainLayout/>,
-        children:[
-            {path:'', element:<HomePage />},
-            {path:'a', element:<APage/>},
-            {path:'b', element:<BPage/>},
-
-        ]
-    },
-])
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router}/>
+    // <RouterProvider router={router}/>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<MainLayout/>}>
+                <Route path={''} element={<HomePage />}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>
 )
