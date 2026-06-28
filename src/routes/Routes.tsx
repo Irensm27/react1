10d137edd8d3cf73a69632ad0d11db2704651e9c
irensm27 <semenets.irina27@gmail.com>
2026-06-28 18:19:09 +0300

    Homework 4

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,6 @@
+import {createBrowserRouter} from "react-router-dom";
+import MainLayout from "../layouts/MainLayout.tsx";
+
+const routes = createBrowserRouter([
+    {path: '/', component: <MainLayout/>,children:
+])
