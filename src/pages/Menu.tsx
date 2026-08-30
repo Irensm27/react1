35b28803b984a099dcf04b4d1db120e45cff2198
irensm27 <semenets.irina27@gmail.com>
2026-08-30 15:28:21 +0300

    Homework 8 start

---
+++ src/pages/Menu.tsx
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom";
+import UsersPage from "./UsersPage.tsx";
+
+
+const Menu = () => {
+    return (
+        <ul>
+            <li><Link to={}></Link></li>
+
+        </ul>
+    );
+};
+
+export default Menu;
